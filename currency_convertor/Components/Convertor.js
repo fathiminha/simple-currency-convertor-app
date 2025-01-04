656b2e6b2af85e24279239f57e399b140c35dcba
Fathima Minha <minhasulaikar@gmail.com>
2025-01-05 02:47:51 +0530

    add some styles to convertor

--- currency_convertor/Components/Convertor.js
+++ currency_convertor/Components/Convertor.js
@@ -1,6 +1,42 @@
+import { StyleSheet } from "react-native"
+import { Button, ToggleButton } from "react-native-paper"
+
 export default function Convertor(){
     return(
         <>
         </>
     )
 }
+
+const styles = StyleSheet.create({
+    header:{
+        flex: 3,
+        width:'100%',
+        alignItems: 'center',
+        padding: 20,
+        marginBottom: 20
+    },
+    body: {
+        flex: 5,
+        width: '100%',
+    },
+    footer: {
+        flex : 2,
+        width: '100%',
+        alignItems: 'center',
+        marginTop: 20
+    },
+    input:{
+        marginBottom: 20
+    },
+    button:{
+        marginVertical: 10,
+    },
+    ToggleButton:{
+        marginTop: 10,
+    },
+    result:{
+        textAlign: 'center',
+        marginTop: 20,
+    }
+})

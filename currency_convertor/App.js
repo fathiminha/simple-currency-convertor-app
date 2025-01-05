4d9169c7c1945d10e15b32f15e97acb8c9a2dffd
Fathima Minha <minhasulaikar@gmail.com>
2025-01-05 15:41:25 +0530

    update app.js

--- currency_convertor/App.js
+++ currency_convertor/App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <SafeAreaProvider>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+          <ScrollView>
+            <View style={styles.container}>
+              <Text>Open up App.js to start working on your app!</Text>
+              <StatusBar style="auto" />
+          </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </SafeAreaProvider>
+    </PaperProvider>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,

38e3a060a0ecf6d73be1ea1bb60a48029ae8dae3
Fathima Minha <minhasulaikar@gmail.com>
2025-01-05 16:00:42 +0530

    updated

--- currency_convertor/App.js
+++ currency_convertor/App.js
@@ -1,30 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Convertor from './Components/Convertor';
 
 export default function App() {
   return (
     <PaperProvider>
       <SafeAreaProvider>
-        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-          <ScrollView>
-            <View style={styles.container}>
-              <Convertor />
-              <StatusBar style="auto" />
-          </View>
-          </ScrollView>
-        </KeyboardAvoidingView>
+        <SafeAreaView>
+          <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <ScrollView >
+              <View style={styles.container}>
+                <Convertor />
+                <StatusBar style="auto" />
+              </View>
+            </ScrollView>
+          </KeyboardAvoidingView>
+        </SafeAreaView>
       </SafeAreaProvider>
     </PaperProvider>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
